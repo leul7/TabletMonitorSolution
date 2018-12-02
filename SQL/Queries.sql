dc75694658ad92667dbaf4d0fcb67d9f0e3fa316
alemneh1 <4alexmic@gmail.com>
2018-12-02 15:13:38 -0800

    Update Queries.sql

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -1,4 +1,17 @@
+/* QUERY 4
+Look up ID, Make, Model for a device, given a device id
+*/
 select ID, Make, Model, Condition
 from DEVICE, DEVICE_MODEL
 where DEVICE.PurchaseDate = DEVICE_MODEL.PurchaseDate 
 AND DEVICE.ID = 9 
+
+/*QUERY 5
+ List all tablets (their Id, make and model) that can be checked out
+ This query lists all tablet that can be checked-out order by purchase date
+*/
+select Device.ID as DeviceID, DEVICE_MODEL.Make, DEVICE_MODEL.Model
+from DEVICE_MODEL, DEVICE
+where DEVICE_MODEL.PurchaseDate = DEVICE.PurchaseDate 
+and device.IsCheckedOut = 0 order by DEVICE_MODEL.PurchaseDate 
+
