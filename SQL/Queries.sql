663b34d2ae9d1055846e388d192ef0da7fade6d3
alemneh1 <4alexmic@gmail.com>
2018-12-02 16:22:51 -0800

    Update Queries.sql

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -1,17 +1,20 @@
-/* QUERY 4
-Look up ID, Make, Model for a device, given a device id
-*/
+/* Query 4 
+Look up ID, Make, Model for a device, given a device id */
 select ID, Make, Model, Condition
 from DEVICE, DEVICE_MODEL
 where DEVICE.PurchaseDate = DEVICE_MODEL.PurchaseDate 
 AND DEVICE.ID = 9 
 
-/*QUERY 5
+/*Query 5
  List all tablets (their Id, make and model) that can be checked out
- This query lists all tablet that can be checked-out order by purchase date
-*/
+ This query lists all tablet that can be checked-out order by purchase date */
 select Device.ID as DeviceID, DEVICE_MODEL.Make, DEVICE_MODEL.Model
 from DEVICE_MODEL, DEVICE
 where DEVICE_MODEL.PurchaseDate = DEVICE.PurchaseDate 
 and device.IsCheckedOut = 0 order by DEVICE_MODEL.PurchaseDate 
 
+/* Query 6
+This query help to determine the number of tablets in the company vs the number of employees. */
+select count(distinct Device.id) as " number of tablets in the company", 
+count(distinct employee.id) as "Number of employee in the company" 
+from Device, employee
